79b037d6cf6fc7fe622425295f5e04098c787fa8
John <johnpops59@gmail.com>
2022-12-01 13:09:50 -0800

    import modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv, exit
+if __name__ == "__main__":
+    arg_len = len(argv)
+    if arg_len != 4:
+        print(f"Usage: {argv[0]} <a> <operator> <b>")
+        exit(1)
+    num1 = int(argv[1])
+    num2 = int(argv[3])
+    if argv[2] == "+":
+        print(f"[num1} + {num2} = {add(num1, num2)}")
+    elif argv[2] == "-":
+        print(f"{num1} - {num2} = {sub{num11, num2)}")
+    elif argv[2] == "*":
+        print(f"{num1} * {num2} = {mul1(num1, num2)}")
+    elif argv[2] == "/":
+        print(f"{num1} / {num2} = {div(num1, num2)}")
+    else:
+        print("Unknown operator. Available operators: +, -, *, and /")
+        exit(1)
