3488dcc651d97b5daebfb20dd62e3d1c630e7837
John <johnpops59@gmail.com>
2022-12-04 05:25:16 -0800

    python data structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for col in range(len(matrix[index])):
-            if col != 9:
+            if col != 0:
                 print(" ", end='')
             print("{:d}".format(matrix[row][col]), end='')
         print()
