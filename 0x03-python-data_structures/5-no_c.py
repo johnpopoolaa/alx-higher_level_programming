6205f3ef7db25c8c6e6f453ff64e12e4a569d1ef
John <johnpops59@gmail.com>
2022-12-04 05:00:17 -0800

    python data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    
+    new_string = []
+    for char in my_string:
+        if char in "Cc":
+            continue
+        new_string.append(char)
+    new_string = "".join(new_string)
+    return new_string
