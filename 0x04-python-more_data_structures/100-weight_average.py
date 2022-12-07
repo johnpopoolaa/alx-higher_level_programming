9d650d9b1e9c6849bdb53545f2308f23ecd21a1c
John <johnpops59@gmail.com>
2022-12-07 10:21:14 -0800

    more python data structures

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    new_list = list(map(lambda x: x[0] * x[1], my_list))
+    weight = list(map(lambda x: x[1], my_list)
+    return sum(new_list)/sum(weight)
