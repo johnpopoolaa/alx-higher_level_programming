afe570e33c22848fd904c59bd57c1beeba7090ef
John <johnpops59@gmail.com>
2022-12-07 07:30:22 -0800

    more data structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,6 +5,6 @@ def search_replace(my_list, search, replace):
     new_list = []
     for element in my_list:
         if element == search:
-            elment = replace
+            element = replace
         new_list.append(element)
     return new_list
