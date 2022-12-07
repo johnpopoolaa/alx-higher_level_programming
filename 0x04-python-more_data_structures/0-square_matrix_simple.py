f9dc8aca197e050cad876018a5b6e5c16b9eed9d
John <johnpops59@gmail.com>
2022-12-07 07:19:58 -0800

    more data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
         return None
     new_list = []
     for row in matrix:
-        inner_list = [x ** 2 for x in row]
+        inner_list = [x * x for x in row]
         new_list.append(inner_list)
     return new_list
