b489eb275500144e32787e7d419b21661809dd17
John Popoola <johnpops59@gmail.com>
2022-12-21 00:07:43 +0100

    first commit

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    from sys import stderr
+    try:
+        result = fct(*args)
+        excapt Exception as e:
+            stderr.write("Exception: {}\n".format(e))
+            result = None
+        return result
