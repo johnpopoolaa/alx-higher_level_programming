fd84ca94606a6074e3e65e1cfe3ce69b86fa2c30
John <johnpops59@gmail.com>
2022-12-21 15:41:51 -0800

    Task 8 solved

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ def safe_function(fct, *args):
     from sys import stderr
     try:
         result = fct(*args)
-        excapt Exception as e:
-            stderr.write("Exception: {}\n".format(e))
-            result = None
-        return result
+    except Exception as e:
+        stderr.write("Exception: {}\n".format(e))
+        result = None
+    return result
