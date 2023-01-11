94898fa9e0b745b1eac6a742427556f63bb80a94
John Popoola <johnpops59@gmail.com>
2023-01-11 23:55:24 +0100

    commit message

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" function to return an object(python data structure) represented by a JSON string
+"""
+
+
+def from_json_string(my_str):
+    """module from_json_string
+        returns python objects
+    """
+    return json.loads(my_str)
