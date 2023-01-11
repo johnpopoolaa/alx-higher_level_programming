6d0e6a1ade358652114dd313ddb8346249666896
John Popoola <johnpops59@gmail.com>
2023-01-11 18:56:47 +0100

    commit message

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,10 @@
-#1/usr/bin/python3
+#!/usr/bin/python3
 """
-funcion that writes a string to a tezt file and returns the number of characters written
+funcion that writes a string to a tezt file and returns
+the number of characters written
 """
+
+
 def write_file(file_name"", text=""):
     """
     module write_file
