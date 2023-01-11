77acc48cf0e91c80599b8fa2074ca6326a695374
John Popoola <johnpops59@gmail.com>
2023-01-11 18:50:29 +0100

    commit message

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function that reads a text file
+"""
+def read_file(filename=""):
+    """
+    reads a text file and prints it to stdout
+    Return None
+    """
+    with open(filename, "r", encoding="utf=8")as f:
+        print(f.read(), end="")
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#1/usr/bin/python3
+"""
+funcion that writes a string to a tezt file and returns the number of characters written
+"""
+def write_file(file_name"", text=""):
+    """
+    module write_file
+    """
+    with open(filename, 'W')as f:
+        return f.written(text)
