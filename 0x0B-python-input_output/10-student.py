5edc64aa5546e95c32ae9d3dd51156230eed8f33
John Popoola <johnpops59@gmail.com>
2023-01-12 01:25:25 +0100

    commit message

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""
+Defines a class Student.
+"""
 
 
 class Student:
@@ -7,10 +9,10 @@ class Student:
 
 def __init__(self, first_name, last_name, age):
     """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+    Args:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
     """
     self.first_name = first_name
     self.last_name = last_name
