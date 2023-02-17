881791f1b5934e0545fb972044bce0eda5405399
John Popoola <johnpops59@gmail.com>
2023-02-18 00:44:46 +0100

    solved tasks

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- this script displays the number of records with '1d = 89' in the table 'first_table' of the database 'hbtn_0c_0' in your MySQL server.
+SELECT COUNT(*) FROM first_table WHERE id=89;
