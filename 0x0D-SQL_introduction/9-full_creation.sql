f27113d131dec642c1e1d88c5ad81afa924589ac
John Popoola <johnpops59@gmail.com>
2023-02-18 01:10:32 +0100

    solved tasks

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- this script creates a table in the datbase in MySQL server and adds multiple rows.
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT, name VARCHAR(256), score INT
+);
+INSERT INTO
+	second_table(id, name, score)
+VALUES
+	(1, "John", 10),
+	(2, "ALex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
