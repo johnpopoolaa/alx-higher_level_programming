412222b3cb7c7477ebb6e0a146861dd0bc811a43
John Popoola <johnpops59@gmail.com>
2023-03-14 02:32:03 +0100

    done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,8 +2,8 @@
 
 const { argv } = require('process');
 if (!argv[2] || isNaN(parseInt(argv[2]))) { console.log('Missing number of occurences'); } else if (parseInt(argv[2]) < 0) { // pass
-} else { 
-	for (let i = 0; i < parseInt(argv[2]); i++) {
-		console.log('C is fun');
-	}
+} else {
+  for (let i = 0; i < parseInt(argv[2]); i++) {
+    console.log('C is fun');
+  }
 }
