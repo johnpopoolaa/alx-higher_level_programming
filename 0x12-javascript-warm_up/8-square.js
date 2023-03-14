965f061fdb3dac405fd36e30278850b0b0d07d3c
John Popoola <johnpops59@gmail.com>
2023-03-14 02:48:55 +0100

    done

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@
 const { argv } = require('process');
 if (!argv[2] || isNaN(parseInt(argv[2]))) { console.log('Missing size'); } else if (parseInt(argv[2]) < 0) { // pass
 } else {
-	for (let i = 0; i < parseInt(argv[2]); i++) {
-		let square = '';
-		for (let j = 0; j< parseInt(argv[2]); j++) { square += 'X'; }
-		console.log(square);
-	}
+  for (let i = 0; i < parseInt(argv[2]); i++) {
+    let square = '';
+    for (let j = 0; j < parseInt(argv[2]); j++) { square += 'X'; }
+    console.log(square);
+  }
 }
