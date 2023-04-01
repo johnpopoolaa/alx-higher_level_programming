0520ab79450995b8914ce0f35fde44b3c6bc1ada
John Popoola <johnpops59@gmail.com>
2023-03-31 20:09:01 -0700

    wrote python script to fetch a url

--- 0x11-python-network_1/0-hbtn_staus.py
+++ 0x11-python-network_1/0-hbtn_staus.py
@@ -4,13 +4,17 @@
 
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-r = urllib.request.Request(url)
+def fetch_url(url):
+    r = urllib.request.Request(url)
 
-with urllib.request.urlopen(url) as response:
-    read_func = response.read()
+    with urllib.request.urlopen(r) as response:
+        read_func = response.read()
 
-print("Body response:")
-print(f"\t- type: {type(read_func)}")
-print(f"\t- content: {read_func}")
-print(f"\t- utf-8 content: {read_func.decode('utf-8')}")
+    print("Body response:")
+    print(f"\t- type: {type(read_func)}")
+    print(f"\t- content: {read_func}")
+    print(f"\t- utf-8 content: {read_func.decode('utf-8')}")
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    fetch_url(url)
